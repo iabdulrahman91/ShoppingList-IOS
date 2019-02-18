729655350e2611d7c45c12d822ff76fff6dd5e85
Abdulrahman Alanazi <iabdulrahman.91@gmail.com>
2019-02-18 04:04:31 -0600

    Initial Commit

---
+++ AlanaziA assignment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlanaziA assignment5
+//
+//  Created by abdulrahman alanazi on 2/18/19.
+//  Copyright © 2019 abdulrahman alanazi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
